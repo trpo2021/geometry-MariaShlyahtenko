99b02ab338f4bb69f59b52e535946d246138565b
MariaShlyahtenko <MariaShkyahtenko@gmail.com>
2021-05-03 19:12:23 +0700

    Modified functions

--- src/libgeometry/functions.h
+++ src/libgeometry/functions.h
@@ -15,11 +15,9 @@ typedef struct {
     double area;
 } figure_circle;
 
-// char* figure_calling(char*, char*, char*);
-// char* search_brackets(char*, char);
-// char* search_digit(char*, double*);
-// char* search_comma(char*);
-// char* end_string(char*);
 double perimeter_circle(figure_circle circle);
 double area_circle(figure_circle circle);
 int test_circle(char*, char*, char*, figure_circle* circle, int);
+int Intersection(
+        figure_circle circle_i, figure_circle circle_j, int num_circle);
+double Lenght_between_points(double x1, double y1, double x2, double y2);
